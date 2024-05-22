c9f837958cf1d2aeebb38e2bb113a8884895b8e9
Etienne <etiennemh3@gmail.com>
2024-05-22 14:32:49 +0200

    creation projet

---
+++ src/main/java/Objects/Cible.java
@@ -0,0 +1,41 @@
+package Objects;
+
+import javafx.scene.image.Image;
+
+public class Cible extends ObjectScheme
+{
+    @Override
+    public void changeImage(Image image) {
+
+    }
+
+    @Override
+    public void changePosition(float positionX, float positionY) {
+
+    }
+
+    @Override
+    public void changeRadiusCommunication(float radius) {
+
+    }
+
+    @Override
+    public float[] getPosition() {
+        return new float[0];
+    }
+
+    @Override
+    public float radiusCommunication() {
+        return 0;
+    }
+
+    @Override
+    public Image getImage() {
+        return null;
+    }
+
+    @Override
+    public boolean isCommunication(ObjectScheme object) {
+        return false;
+    }
+}
