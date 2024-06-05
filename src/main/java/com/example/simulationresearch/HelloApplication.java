463a8bf277bc6c94c330585563a34f697db8a882
Paulo <paulojuniorgomescosta@gmail.com>
2024-06-05 13:00:44 +0200

    Pablo

--- src/main/java/com/example/simulationresearch/HelloApplication.java
+++ src/main/java/com/example/simulationresearch/HelloApplication.java
@@ -22,21 +22,27 @@ import static LectureConfig.LectureConfig.agentsCommunicationRange;
 import static LectureConfig.LectureConfig.agentsDetectionRange;
 import static com.example.simulationresearch.InterfaceController.*;
 
+import com.example.simulationresearch.InterfaceController.*;
+
 public class HelloApplication extends Application {
     static File file;
     static int Demarage = 0;
     @Override
     public void start(Stage stage) throws IOException, InterruptedException {
 
+        //Lis le fichier de base
         file = new File("src/main/resources/configuration.txt");
         LectureConfig.LectureFichier(file);
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Hello-view.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load(),1100,800);
         stage.setTitle("Spaceship");
         stage.setScene(scene);
 
         HelloController Ctrl_Global = fxmlLoader.getController();
+
+
 
         final long startNanoTime = System.nanoTime();
         System.out.println(startNanoTime);
@@ -46,14 +52,16 @@ public class HelloApplication extends Application {
         {
             public void handle(long currentNanoTime)
             {
-                LectureConfig.LectureFichier(file);
+                //LectureConfig.LectureFichier(file);
+
                 if(Situation == 1) {
-                    //LectureConfig.LectureFichier(file);
+                    //Afficher les objets et l'animation
                     Ctrl_Global.Afficher(BackGround);
                 }
                 else if(Situation == 2){
-                    //LectureConfig.LectureFichier(file);
+                    //Recrer les objets a leurs positions initial
                     GestionObjects.creationObjects(5,imageAgent, imageTarget);
+                    //Affiche les nouveaux objet a leur position initial en boucle tant que Situation rest a 2
                     Ctrl_Global.Afficher(BackGround);
                 }
                 else{
@@ -61,6 +69,7 @@ public class HelloApplication extends Application {
                 }
             }
         }.start();
+
         stage.show();
     }
 
--- src/main/java/com/example/simulationresearch/HelloController.java
+++ src/main/java/com/example/simulationresearch/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.simulationresearch;
 
 import GestionObjects.GestionObjects;
+import LectureConfig.LectureConfig;
+import com.example.simulationresearch.InterfaceController;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.canvas.Canvas;
@@ -10,7 +13,11 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.File;
 import java.io.IOException;
+
+import static LectureConfig.LectureConfig.*;
+import static com.example.simulationresearch.InterfaceController.*;
 
 
 public class HelloController {
@@ -19,7 +26,6 @@ public class HelloController {
 
 
     public void Afficher(Image bg){
-
         GraphicsContext gc = myCanvas.getGraphicsContext2D();
         for(int idx = 0; idx < GestionObjects.NbrAgent; idx++)
         {
@@ -29,4 +35,15 @@ public class HelloController {
         GestionObjects.Affichage(gc, bg);
     }
 
+    /*
+    public void affichageDataFile(){
+        FXMLLoader fxmlLoaderInterface = new FXMLLoader(HelloApplication.class.getResource("Interface.fxml"));
+        InterfaceController Ctrl_Interface = fxmlLoaderInterface.getController();
+
+        Ctrl_Interface.setLbl_PositionTarget("Position of the target : (" + posCible[0] + ";" + posCible[1] + ")");
+        Ctrl_Interface.setLbl_TargetComm("Radius communication target : " + agentsDetectionRange);
+        Ctrl_Interface.setLbl_AgentComm("Radius communication agent : " + agentsCommunicationRange);
+    }
+
+     */
 }
